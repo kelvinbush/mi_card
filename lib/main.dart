a5fe904c7056dc07ba8508978d3839a5d780396e
Kelvin Bush <kelybush@gmail.com>
2021-07-08 15:30:07 +0300

    adding avatar
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -14,31 +14,14 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.white,
-                child: Text("Container 1"),
-              ),
-              SizedBox(width: 20.0),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.blue,
-                child: Text("Container 2"),
-              ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.red,
-                child: Text("Container 3"),
-              )
-            ],
-          ),
-        ),
+            child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/kelvin.jpg'),
+            ),
+          ],
+        )),
       ),
     );
   }
